94256f2a5ad1a9ea9a5c74c3d6140c42581128d8
Brothre23 <daveyu824@gmail.com>
2021-04-27 19:16:59 +0800

    refactoring of initramfs

---
+++ initramfs/fork_test/code/fork_test.c
@@ -0,0 +1,25 @@
+#include "printf.h"
+#include "uart.h"
+#include "syscall.h"
+
+void main(int argc, char **argv)
+{
+    init_printf(0, putc);
+    // while (1)
+    int counter = 4;
+    while (counter--)
+    {
+        for (int i = 0; i < 10000000; i++)
+        {
+            if (i % 2000000 == 0)
+            {
+                printf("[child] pid: %d %d\n", getpid(), i);
+                printf("%d ", argc);
+                for (int j = 0; j < argc; j++)
+                    printf("%s ", argv[j]);
+                printf("\n");
+            }
+        }
+    }
+    exit();
+}
